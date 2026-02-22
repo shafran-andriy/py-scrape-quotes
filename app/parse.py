5846d684fb5f5af75f23ee53d34ab09f00fb27a7
shafran <shafran.andriy@gmail.com>
2026-02-22 20:24:52 +0200

    fix flake8 issues

--- app/parse.py
+++ app/parse.py
@@ -23,13 +23,14 @@ def parse_single_quote(quote: Tag) -> Quote:
         author=quote.select_one(".author").text,
         tags=[tag.text for tag in quote.select(".tags .tag")]
     )
-    
+
 
 def get_home_quotes() -> [Quote]:
     text = requests.get(BASE_URL).content
     soup = BeautifulSoup(text, "html.parser")
     products = soup.select(".quote")
     return [parse_single_quote(product) for product in products]
+
 
 def get_num_pages() -> int:
     num_of_pages = 0
@@ -43,6 +44,7 @@ def get_num_pages() -> int:
         i += 1
     return num_of_pages
 
+
 def get_all_quotes() -> [Quote]:
     all_quotes = []
     num_of_pages = get_num_pages()
@@ -50,7 +52,7 @@ def get_all_quotes() -> [Quote]:
         logging.info(F"Start parsing page #{i}")
         all_quotes.extend(get_home_quotes())
     return all_quotes
-    
+  
 QUOTE_FIELDS = [field.name for field in fields(Quote)]
 
 logging.basicConfig(
@@ -62,11 +64,13 @@ logging.basicConfig(
     ]
     )
 
+
 def write_quotes_to_csv(quotes: [Quote]) -> None:
     with open("quotes.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerow(QUOTE_FIELDS)
         writer.writerows([astuple(quote) for quote in quotes])
+
 
 def main(output_csv_path: str) -> None:
     write_quotes_to_csv(get_all_quotes())
@@ -74,4 +78,3 @@ def main(output_csv_path: str) -> None:
 
 if __name__ == "__main__":
     main("quotes.csv")
-
